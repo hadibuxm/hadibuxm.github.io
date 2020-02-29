7eb86feb24887c1ab5410d66583aa59361b3e728
hadi2211 <mahessarm@gmail.com>
2020-02-29 19:35:55 +0500

    modified

---
+++ main.js
@@ -0,0 +1,12 @@
+function grow() {
+    document.getElementById("social").style.fontSize = "50px";
+    document.getElementById("footer").style.backgroundColor = "white";
+    document.getElementById("social").style.color = "black";
+}
+
+function same() {
+    document.getElementById("footer").style.backgroundColor = "black"
+    document.getElementById("social").style.fontSize = "40px";
+    document.getElementById("social").style.color = "white";
+
+}
